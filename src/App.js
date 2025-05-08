62e32231a26028c63cea981a5b5fe6fbb4e8c318
skyjirayudev <skyjirayusaisuwan@gmail.com>
2025-05-09 00:12:35 +1200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
-import {
-  Route, 
-  Routes
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import pages from './utils/pages';
 import Layout from './components/layout/Layout';
 import Home from './components/pages/Home';
@@ -12,35 +9,18 @@ import UnderConstruction from './components/pages/UnderConstruction';
 
 const App = () => {
   return (
-    <>
-      <Layout>
-        <Routes>
-          <Route path={pages.get('home').path} element={<Home />} />
-          <Route 
-            path={pages.get('about').path} 
-            element={<UnderConstruction />} 
-          />
-          <Route 
-            path={pages.get('menu').path} 
-            element={<UnderConstruction />} 
-          />
-          <Route path={pages.get('bookings').path} element={<Bookings />} />
-          <Route 
-            path={pages.get('confirmedBooking').path} 
-            element={<ConfirmedBooking />} 
-          />
-          <Route 
-            path={pages.get('orderOnline').path} 
-            element={<UnderConstruction />} 
-          />
-          <Route 
-            path={pages.get('login').path} 
-            element={<UnderConstruction />} 
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Layout>
-    </>
+    <Layout>
+      <Routes>
+        <Route path={pages.get('home').path} element={<Home />} />
+        <Route path={pages.get('about').path}         element={<UnderConstruction />} />
+        <Route path={pages.get('menu').path}          element={<UnderConstruction />} />
+        <Route path={pages.get('orderOnline').path}   element={<UnderConstruction />} />
+        <Route path={pages.get('login').path}         element={<UnderConstruction />} />
+        <Route path={pages.get('bookings').path} element={<Bookings />} />
+        <Route path={pages.get('confirmedBooking').path} element={<ConfirmedBooking />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
   );
 };
 
